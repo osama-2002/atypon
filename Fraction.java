dfc2aa9fdbcec9fe5babc8ac579ae1e6577baed1
Osama Almelhem <osamaalmelhem2002@gmail.com>
2025-01-14 09:55:37 +0300

    project kick off

---
+++ Fraction.java
@@ -0,0 +1,13 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void print() {
+        System.out.println(numerator + " / " + denominator);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(8,2);
+        f1.print();
+    }
+}
