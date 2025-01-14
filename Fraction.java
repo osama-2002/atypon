24053fe7aefb44eff5b6c4336274cdad6951de78
Osama Almelhem <osamaalmelhem2002@gmail.com>
2025-01-14 10:00:21 +0300

    feature 2

--- Fraction.java
+++ Fraction.java
@@ -15,7 +15,19 @@ public class Fraction {
         return new Fraction(numerator * fraction.denominator - denominator * fraction.numerator, denominator * fraction.denominator);
     }
 
+    private int gcd(int n, int d) {
+        if(d==0) return n;
+        return gcd(d, n%d);
+    }
+
+    private void simplify() {
+        int gcd = gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
     public void print() {
+        simplify();
         System.out.println(numerator + " / " + denominator);
     }
 }
